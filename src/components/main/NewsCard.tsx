38a3ecabe61c8c16d69ffcd0d24a940eba7bb802
89882 <pgh123166@naver.com>
2023-05-08 23:19:55 +0900

    feat: 복지 소식 카드 구현

---
+++ src/components/main/NewsCard.tsx
@@ -0,0 +1,10 @@
+export default function NewsCard() {
+  return (
+    <div className="bg-cardColor rounded-[10px] shadow-lg w-[20.5rem] h-[30rem] flex justify-center items-center">
+      <div className="w-[18rem] h-[26.8rem]  flex flex-col items-center justify-between">
+        <text className="font-bold text-[28px] font-inter">복지 소식</text>
+        <div className="w-[16.3rem] h-[20.3rem]  mt-2 mb-7  bg-gray-400"></div>
+      </div>
+    </div>
+  );
+}
