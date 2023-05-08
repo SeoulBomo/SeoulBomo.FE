2f709ffd60b3e24c237ead3a03d298ec3d191bf4
89882 <pgh123166@naver.com>
2023-05-08 20:04:53 +0900

    feat: popularCard 구현

--- src/components/main/CardFrame.tsx
+++ src/components/main/CardFrame.tsx
@@ -1,10 +1,12 @@
 import CalculateCard from "./CalculateCard";
+import PopularCard from "./PopularCard";
 
 export default function CardFrame() {
   return (
     <div className="bg-cardFrameColor  shadow-lg w-[80.75rem] h-[45rem] flex  items-center justify-center">
-      <div className="h-[35rem] w-[72rem] flex items-center">
-        <CalculateCard></CalculateCard>
+      <div className="h-[35rem] w-[72rem] flex items-center justify-evenly">
+        <CalculateCard />
+        <PopularCard />
       </div>
     </div>
   );
