722ef68d53692518810e6ff2edd8a764194f19b9
89882 <pgh123166@naver.com>
2023-05-11 12:05:27 +0900

    refactor: props->객체 변경

--- src/components/information/InformationCard.tsx
+++ src/components/information/InformationCard.tsx
@@ -1,9 +1,9 @@
-export default function InformationCard(props: propsType) {
+export default function InformationCard({ data, text }: propsType) {
   return (
     <section className="bg-white w-[80.25rem] h-[38rem] mb-[5rem] rounded-[1.25rem] drop-shadow-xl px-[7rem] pt-[3rem]">
-      <text className="font-bold text-3xl">{props.text}</text>
+      <text className="font-bold text-3xl">{text}</text>
       <div className="grid grid-cols-2 gap-10 mt-[3rem]">
-        {props.data.map((item: any) => (
+        {data?.map((item: any) => (
           <div
             key={item.id}
             className="w-[29.625rem] h-[4.1rem] rounded-[10px] bg-amber-200 font-semibold text-2xl flex items-center hover:translate-x-3 ease-in-out duration-300"
