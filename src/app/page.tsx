07d135936375ac8fb219aa2e479e09e7fc6f1db4
김태강 <k_2684@naver.com>
2023-05-15 22:51:39 +0900

    chore: 전체 페이지 스타일 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,13 @@
-"use client";
 import CardFrame from "@/components/main/CardFrame";
 import FilterSection from "@/components/main/FilterSection";
 import SearchSection from "@/components/main/SearchSection";
 
 export default function Home() {
   return (
-    <>
-      <div className="bg-cardFrameColor shadow-lg w-screen h-screen flex justify-center items-center flex-wrap">
-        <SearchSection />
-        <FilterSection />
-      </div>
-      <div className="w-screen flex justify-center items-center">
-        <CardFrame />
-      </div>
-    </>
+    <div className="bg-cardFrameColor shadow-lg flex justify-center items-center flex-wrap">
+      <SearchSection />
+      <FilterSection />
+      <CardFrame />
+    </div>
   );
 }
