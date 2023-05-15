37908f5bb3cfb408b5b6b933983e067d78b892da
김태강 <k_2684@naver.com>
2023-05-15 22:51:27 +0900

    feat: searchSection 반응형 구현

--- src/components/main/SearchSection.tsx
+++ src/components/main/SearchSection.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function SearchSection() {
   return (
-    <div className="p-[3rem] bg-white rounded-[1.375rem] drop-shadow-xl flex justify-center items-center">
+    <div className="p-[2rem] lg:p-[3rem] my-[4rem] bg-white rounded-[1.375rem] drop-shadow-xl flex justify-center items-center">
       <div>
         <div className=" pb-[2rem]">
           <text className="font-semibold text-[0.8rem] md:text-base">
@@ -20,10 +20,10 @@ export default function SearchSection() {
             type="text"
             name="search"
             id="search"
-            className="placeholder:text-gray-400 w-[10rem] sm:w-[20rem] lg:w-[40rem] py-[0.7rem] border-4 border-yellowColor rounded-[28px] focus:outline-none focus:border-yellowColor focus:ring-1 focus:ring-yellowColor pl-[1rem] text-sm md:text-xl"
+            className="placeholder:text-gray-400 w-[12rem] sm:w-[20rem] lg:w-[40rem] py-[0.7rem] border-4 border-yellowColor rounded-[28px] focus:outline-none focus:border-yellowColor focus:ring-1 focus:ring-yellowColor pl-[1rem] text-sm md:text-xl"
           ></input>
-          <button className="px-[1rem] py-[0.5rem] bg-yellowColor rounded-[0.9rem] ml-[0.5rem] text-sm font-bold hover:bg-amber-200">
-            Search
+          <button className="px-[1rem] py-[0.5rem] bg-yellowColor rounded-[0.9rem] ml-[0.5rem] text-xl lg:text-2xl font-bold hover:bg-amber-200">
+            🔍
           </button>
         </div>
       </div>
