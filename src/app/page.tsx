4abd04027c811ca2e458b2717623cc0d62513bec
김태강 <k_2684@naver.com>
2023-05-17 03:12:18 +0900

    chore: 메인페이지 map 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import CardFrame from "@/components/main/CardSection/CardFrame";
 import FilterSection from "@/components/main/FilterSection/FilterSection";
 import SearchSection from "@/components/main/SearchSection/SearchSection";
+import MapFrame from "@/components/main/map/MapFrame";
 
 export default function Home() {
   return (
     <div className="bg-cardFrameColor shadow-lg flex justify-center items-center flex-wrap">
       <SearchSection />
       <FilterSection />
+      <MapFrame />
       <CardFrame />
     </div>
   );
