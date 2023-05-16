f8affb8621c042d53e6c8cbab3ced77cb88dd4fc
김태강 <k_2684@naver.com>
2023-05-16 16:51:46 +0900

    chore: 헤더 컴포넌트 이름 변경

---
+++ src/app/search/page.tsx
@@ -0,0 +1,9 @@
+import MainSearchFrame from "@/components/mainSearch/MainSearchFrame";
+
+export default function Home() {
+  return (
+    <div className="bg-cardFrameColor shadow-lg flex justify-center items-center flex-wrap">
+      <MainSearchFrame />
+    </div>
+  );
+}
