79baebff12bb81d0dd4557abb1d0cf18772fdc72
김태강 <k_2684@naver.com>
2023-05-17 16:13:31 +0900

    feat: searchFrame 반응형 구현

--- src/components/mainSearch/MainSearchFrame.tsx
+++ src/components/mainSearch/MainSearchFrame.tsx
@@ -26,26 +26,28 @@ export default function MainSearchFrame() {
   ];
   //dummy 데이터
   return (
-    <main className="w-screen flex flex-col items-center justify-between">
-      <div className="mt-[3rem]">
-        <text className="text-xl font-medium">
+    <main className="flex flex-col items-center justify-between">
+      <div className="flex flex-col pt-[3rem]">
+        <text className="ml-[1rem] text-[0rem] sm:text-xl font-medium">
           검색을 통해 각종 정보를 만나볼 수 있습니다
         </text>
-        <section className="bg-yellowColor w-[46.8rem] h-[5rem] rounded-[1.3rem] mt-[0.5rem] flex items-center justify-evenly">
+        <section className="bg-yellowColor px-[1rem] sm:px-[2rem] h-[4rem] sm:h-[5rem] rounded-[1.3rem] mt-[0.5rem] flex justify-center items-center gap-3">
           <input
             type="text"
             name="search"
             id="search"
-            className="placeholder:text-gray-400  w-[33.75rem] h-[2.5rem] border-4 border-white rounded-[28px]   focus:outline-none  pl-[1rem] text-xl"
+            className="placeholder:text-gray-400 pl-[1rem] w-[12rem] h-[2rem] sm:w-[20rem] sm:h-[2.5rem] md:w-[28rem] lg:w-[33rem]border-4 border-white rounded-[28px] focus:outline-yellow-300  p  -[1rem] text-xl"
           ></input>
-          <button className="w-[6.125rem] h-[2.5rem] bg-white rounded-[0.9rem] ml-[0.3rem] text-sm font-bold hover:bg-gray-100">
+          <button className="w-[4rem] sm:w-[6.125rem] h-[2rem] sm:h-[2.5rem] bg-white rounded-[0.9rem] text-xs sm:text-sm font-bold hover:bg-gray-100">
             Search
           </button>
         </section>
       </div>
-      <SearchCard category="복지서비스" data={dummy} />
-      <SearchCard category="복지시설" data={dummy} />
-      <SearchCard category="정책" data={dummy} />
+      <div className="my-[4rem] flex flex-col gap-10">
+        <SearchCard category="복지서비스" data={dummy} />
+        <SearchCard category="복지시설" data={dummy} />
+        <SearchCard category="정책" data={dummy} />
+      </div>
     </main>
   );
 }
