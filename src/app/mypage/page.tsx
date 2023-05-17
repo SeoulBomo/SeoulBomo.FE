14ee57ecb2f903e085d1bbb32c68035942aedf22
김태강 <k_2684@naver.com>
2023-05-17 23:26:30 +0900

    chore: mypage 라우팅

---
+++ src/app/mypage/page.tsx
@@ -0,0 +1,9 @@
+import MypageFrame from "@/components/my/MypageFrame";
+
+export default function MyPage() {
+  return (
+    <div className="bg-cardFrameColor flex justify-start items-center flex-wrap">
+      <MypageFrame />
+    </div>
+  );
+}
