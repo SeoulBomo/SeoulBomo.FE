beebd112f05a530982602afdeabf117bd354cf3e
김태강 <k_2684@naver.com>
2023-05-18 13:55:05 +0900

    chore: information 라우팅

---
+++ src/app/information/page.tsx
@@ -0,0 +1,9 @@
+import InformationFrame from "@/components/information/InformationFrame";
+
+export default function Map() {
+  return (
+    <div className="bg-cardFrameColor flex justify-center items-center flex-wrap">
+      <InformationFrame />
+    </div>
+  );
+}
