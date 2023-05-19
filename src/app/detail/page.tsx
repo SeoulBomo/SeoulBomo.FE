001c69bb0b848664b41c2f5ab409e969df8d5bc9
89882 <pgh123166@naver.com>
2023-05-20 01:27:52 +0900

    feat: 상세 페이지 구현

---
+++ src/app/detail/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+import DetailFrame from "@/components/detail/DetailFrame";
+export default function Detail() {
+  return (
+    <div className="bg-cardFrameColor w-screen h-screen">
+      <DetailFrame />
+    </div>
+  );
+}
