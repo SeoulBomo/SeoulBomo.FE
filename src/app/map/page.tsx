0bc21185092c9d260d3e77cbc493fac24312bd25
김태강 <k_2684@naver.com>
2023-05-21 00:29:30 +0900

    chore: map 임시 라우팅 삭제

--- src/app/map/page.tsx
+++
@@ -1,9 +0,0 @@
-import MapSearchFrame from "@/components/mapSearch/MapSearchFrame";
-
-export default function Map() {
-  return (
-    <div className="flex justify-center items-center flex-wrap">
-      <MapSearchFrame />
-    </div>
-  );
-}
