888e2e9a4e7cbef9f1840f51f194a404d2021982
89882 <pgh123166@naver.com>
2023-05-22 16:27:01 +0900

    feat: react query 구현

--- src/components/main/CardSection/PopularCard.tsx
+++ src/components/main/CardSection/PopularCard.tsx
@@ -1,44 +1,46 @@
+"use client";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
 export default function PopularCard() {
-  const dummy = [
+  const getPopularCard = async () => {
+    const { data } = await axios.get("/dummy/popularCardDummy.json");
+    return data;
+  };
+
+  const { isLoading, isError, data, error } = useQuery(
+    ["popularCard"],
+    getPopularCard,
     {
-      id: 1,
-      name: "오늘 어린이 집에서 ~",
-    },
-    {
-      id: 2,
-      name: "오늘 어린이 집에서 ~",
-    },
-    {
-      id: 3,
-      name: "오늘 어린이 집에서 ~",
-    },
-    {
-      id: 4,
-      name: "오늘 어린이 집에서 ~",
-    },
-    {
-      id: 5,
-      name: "오늘 어린이 집에서 ~",
-    },
-    {
-      id: 6,
-      name: "오늘 어린이 집에서 ~",
-    },
-    {
-      id: 7,
-      name: "오늘 어린이 집에서 가나다라마바사 아자차카",
-    },
-  ];
+      refetchOnWindowFocus: false, // react-query는 사용자가 사용하는 윈도우가 다른 곳을 갔다가 다시 화면으로 돌아오면 이 함수를 재실행합니다. 그 재실행 여부 옵션 입니다.
+      retry: 0, // 실패시 재호출 몇번 할지
+      onSuccess: (data: any) => {
+        // 성공시 호출
+        console.log(data);
+        console.log("성공입니다");
+      },
+      onError: ({ e }: any) => {
+        console.log(e.message);
+      },
+    }
+  );
+  if (isLoading) {
+    return <span>Loading...</span>;
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>;
+  }
   return (
     <div className="bg-yellowColor rounded-[1rem] flex flex-col items-center shadow-lg p-[1rem] w-[18rem] lg:w-[20.5rem] lg:h-[30rem]">
-      <text className="font-bold text-[1.5rem] lg:text-[1.75rem] mb-[1rem]">
+      <div className="font-bold text-[1.5rem] lg:text-[1.75rem] mb-[1rem]">
         인기 복지
-      </text>
+      </div>
       <div className="flex flex-col justify-evenly w-[14rem] h-[28rem] lg:w-[18rem] lg:h-[26rem] p-[0.5rem] rounded-[1rem] bg-white">
-        {dummy?.map((item) => (
+        {data.list?.map((item: any) => (
           <text
             key={item.id}
-            className="font-medium text-lg lg:text-2xl truncate hover:underline underline-offset-4 cursor-pointer"
+            className="font-medium text-lg lg:text-xl truncate hover:underline underline-offset-4 cursor-pointer"
           >
             {item.name}
           </text>
