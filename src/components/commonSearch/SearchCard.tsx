f73e0421730048326a97d05cecfc9f16cdb00082
89882 <pgh123166@naver.com>
2023-05-23 17:00:08 +0900

    refactor: 목록 데이터 없을 시 대체 ui 구현
    Co-authored-by: grayashh <k_2684@naver.com>

--- src/components/commonSearch/SearchCard.tsx
+++ src/components/commonSearch/SearchCard.tsx
@@ -4,32 +4,36 @@ export default function SearchCard({ category, data }: propsType) {
   return (
     <section className="flex flex-col w-[20rem] sm:w-[40rem] md:w-[50rem] lg:w-[60rem] border-2 border-gray-200 bg-white rounded-[1rem] drop-shadow-[0_1.5rem__1.5rem_rgba(0,0,0,0.05)] hover:drop-shadow-[0_2rem_2rem_rgba(0,0,0,0.07)] p-[3rem]">
       <div className="rounded=[0.625rem] sm:w-[11.6rem] h-[3rem] sm:h-[5rem] bg-amber-200 flex justify-center items-center font-bold text-2xl mb-[1rem] rounded-[0.625rem]">
-        <text className="text-xl sm:text-2xl">{category}</text>
+        <div className="text-xl sm:text-2xl">{category}</div>
       </div>
       <ul role="list" className="divide-y divide-gray-100">
-        {data?.map((item: any) => (
-          <li
-            key={item.id}
-            className="flex flex-col justify-between gap-x-4 py-[2rem]"
-          >
-            <div className="flex flex-col cursor-pointer">
-              <p className="text-lg sm:text-xl font-semibold text-gray-900 truncate pb-[0.25rem]">
-                {item.name}
-              </p>
-              <p className="mt-1 truncate text-sm sm:text-md leading-5 text-gray-500">
-                {item.address}
-              </p>
-            </div>
-          </li>
-        ))}
+        {data.length !== 0 ? (
+          data?.map((item: any) => (
+            <li
+              key={item.id}
+              className="flex flex-col justify-between gap-x-4 py-[2rem]"
+            >
+              <div className="flex flex-col cursor-pointer">
+                <p className="text-lg sm:text-xl font-semibold text-gray-900 truncate pb-[0.25rem]">
+                  {item.name}
+                </p>
+                <p className="mt-1 truncate text-sm sm:text-md leading-5 text-gray-500">
+                  {item.address}
+                </p>
+              </div>
+            </li>
+          ))
+        ) : (
+          <div>검색어가 존재하지 않습니다</div>
+        )}
       </ul>
       <Link href="/search-list">
         {data.length >= 3 ? (
-          <text className="font-normal text-xs sm:text-sm text-gray-400 hover:underline underline-offset-4 hover:cursor-pointer">
+          <div className="font-normal text-xs sm:text-sm text-gray-400 hover:underline underline-offset-4 hover:cursor-pointer">
             내용 더보기
-          </text>
+          </div>
         ) : (
-          <text></text>
+          <div></div>
         )}
       </Link>
     </section>
