134a443bfa8f5cb21b25e8e9bb40ee791aebf313
김태강 <k_2684@naver.com>
2023-05-25 18:15:48 +0900

    chore: Oauth URL 수정

--- src/components/login/Oauth.ts
+++ src/components/login/Oauth.ts
@@ -1,11 +1,7 @@
-const KAKAO_CLIENT_ID = "723304452fed4f3c110678180f09ece6";
-const KAKAO_REDIRECT_URI = "http://localhost:3000/oauth/kakao";
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}&response_type=code`;
 
-export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
+export const KAKAO_TOKEN_URL = `https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}&code=`;
 
-const NAVER_CLIENT_ID = "YDmaPr5AwyhBuTHwoUUM";
-const NAVER_CLIENT_SECRET = "MDuxZLS3rQ";
-const NAVER_STATE = "seoulbomo";
-const NAVER_REDIRECT_URI = "http://localhost:300  0/oauth/naver";
+export const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_NAVER_CLIENT_ID}&state=${process.env.NEXT_PUBLIC_NAVER_STATE}&redirect_uri=${process.env.NEXT_PUBLIC_NAVER_REDIRECT_URI}`;
 
-export const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&state=${NAVER_STATE}&redirect_uri=${NAVER_REDIRECT_URI}`;
+export const NAVER_TOKEN_URL = `https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id=${process.env.NEXT_PUBLIC_NAVER_CLIENT_ID}&client_secret=${process.env.NEXT_PUBLIC_NAVER_CLIENT_SECRET}&state=${process.env.NEXT_PUBLIC_NAVER_STATE}&code=`;
