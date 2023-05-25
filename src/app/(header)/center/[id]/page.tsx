5f538a049068cecd6cd575446bbc1956ef9ffb0f
89882 <pgh123166@naver.com>
2023-05-25 18:13:43 +0900

    chore: detail page ur 수정

---
+++ src/app/(header)/center/[id]/page.tsx
@@ -0,0 +1,9 @@
+import DetailFrame from "@/components/detail/DetailFrame";
+
+export default function Search() {
+  return (
+    <div className="flex min-h-screen justify-center flex-wrap">
+      <DetailFrame />
+    </div>
+  );
+}
