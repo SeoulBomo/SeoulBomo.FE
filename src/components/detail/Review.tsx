5f31f9b4c3c11a8e84cd0f4704445873f00e3121
김태강 <k_2684@naver.com>
2023-05-26 17:35:04 +0900

    feat: 리뷰 탭 구현

---
+++ src/components/detail/Review.tsx
@@ -0,0 +1,398 @@
+import { Menu, Transition } from "@headlessui/react";
+import { Fragment, useEffect, useRef, useState } from "react";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+
+export default function Review() {
+  return (
+    <section className="bg-white py-8 lg:py-16">
+      <div className="max-w-2xl mx-auto px-4">
+        <div className="flex justify-between items-center mb-6">
+          <h2 className="text-lg lg:text-2xl font-bold text-gray-900">
+            리뷰 (20)
+          </h2>
+        </div>
+        <form className="mb-6">
+          <div className="py-2 px-4 mb-4 bg-white rounded-lg rounded-t-lg border border-gray-200">
+            <label className="sr-only">Your comment</label>
+            <textarea
+              id="comment"
+              rows={6}
+              className="px-0 w-full text-sm text-gray-900 border-0 focus:ring-0 focus:outline-none"
+              placeholder="리뷰를 작성해주세요!"
+              required
+            ></textarea>
+          </div>
+          <div className="flex flex-1 justify-end">
+            <button
+              type="submit"
+              className="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-yellow-400 rounded-lg focus:ring-4 focus:ring-yellow-200 hover:bg-yellow-4  00"
+            >
+              리뷰 남기기
+            </button>
+          </div>
+        </form>
+        <article className="p-6 mb-6 text-base bg-white rounded-lg">
+          <footer className="flex justify-between items-center mb-2">
+            <div className="flex items-center">
+              <p className="inline-flex items-center mr-3 text-sm text-gray-900">
+                <img
+                  className="mr-2 w-6 h-6 rounded-full"
+                  src="https://flowbite.com/docs/images/people/profile-picture-2.jpg"
+                  alt="Michael Gough"
+                />
+                Michael Gough
+              </p>
+              <p className="text-sm text-gray-600">
+                <time dateTime="2021-02-14" title="February 8th, 2022">
+                  Feb. 8, 2022
+                </time>
+              </p>
+            </div>
+            <Menu as="div" className="relative inline-block text-left">
+              <div>
+                <Menu.Button className="inline-flex items-center p-2 text-sm font-medium text-center text-gray-400 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50">
+                  <svg
+                    className="w-5 h-5"
+                    aria-hidden="true"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z"></path>
+                  </svg>
+                </Menu.Button>
+              </div>
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+              >
+                <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                  <div className="px-1 py-1 ">
+                    <Menu.Item>
+                      {({ active }) => (
+                        <button
+                          className={`${
+                            active
+                              ? "bg-yellow-300 text-white"
+                              : "text-gray-900"
+                          } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                        >
+                          {active ? (
+                            <EditActiveIcon
+                              className="mr-2 h-5 w-5"
+                              aria-hidden="true"
+                            />
+                          ) : (
+                            <EditInactiveIcon
+                              className="mr-2 h-5 w-5"
+                              aria-hidden="true"
+                            />
+                          )}
+                          수정
+                        </button>
+                      )}
+                    </Menu.Item>
+                  </div>
+                  <div className="px-1 py-1">
+                    <Menu.Item>
+                      {({ active }) => (
+                        <button
+                          className={`${
+                            active
+                              ? "bg-yellow-300 text-white"
+                              : "text-gray-900"
+                          } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                        >
+                          {active ? (
+                            <DeleteActiveIcon
+                              className="mr-2 h-5 w-5 text-yellow-400"
+                              aria-hidden="true"
+                            />
+                          ) : (
+                            <DeleteInactiveIcon
+                              className="mr-2 h-5 w-5 text-yellow-400"
+                              aria-hidden="true"
+                            />
+                          )}
+                          삭제
+                        </button>
+                      )}
+                    </Menu.Item>
+                  </div>
+                </Menu.Items>
+              </Transition>
+            </Menu>
+          </footer>
+          <p className="text-gray-500">
+            Very straight-to-point article. Really worth time reading. Thank
+            you! But tools are just the instruments for the UX designers. The
+            knowledge of the design tools are as important as the creation of
+            the design strategy.
+          </p>
+          <div className="flex items-center mt-4 space-x-4"></div>
+        </article>
+        <article className="p-6 mb-6 text-base bg-white border-t border-gray-200">
+          <footer className="flex justify-between items-center mb-2">
+            <div className="flex items-center">
+              <p className="inline-flex items-center mr-3 text-sm text-gray-900">
+                <img
+                  className="mr-2 w-6 h-6 rounded-full"
+                  src="https://flowbite.com/docs/images/people/profile-picture-3.jpg"
+                  alt="Bonnie Green"
+                />
+                Bonnie Green
+              </p>
+              <p className="text-sm text-gray-600">
+                <time dateTime="2022-03-12" title="March 12th, 2022">
+                  Mar. 12, 2022
+                </time>
+              </p>
+            </div>
+            <button
+              id="dropdownComment3Button"
+              data-dropdown-toggle="dropdownComment3"
+              className="inline-flex items-center p-2 text-sm font-medium text-center text-gray-400 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50"
+              type="button"
+            >
+              <svg
+                className="w-5 h-5"
+                aria-hidden="true"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z"></path>
+              </svg>
+              <span className="sr-only">Comment settings</span>
+            </button>
+            <div
+              id="dropdownComment3"
+              className="hidden z-10 w-36 bg-white rounded divide-y divide-gray-100 shadow"
+            >
+              <ul
+                className="py-1 text-sm text-gray-700"
+                aria-labelledby="dropdownMenuIconHorizontalButton"
+              >
+                <li>
+                  <a href="#" className="block py-2 px-4 hover:bg-gray-100">
+                    Edit
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="block py-2 px-4 hover:bg-gray-100">
+                    Remove
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="block py-2 px-4 hover:bg-gray-100">
+                    Report
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </footer>
+          <p className="text-gray-500">
+            The article covers the essentials, challenges, myths and stages the
+            UX designer should consider while creating the design strategy.
+          </p>
+          <div className="flex items-center mt-4 space-x-4">
+            <button
+              type="button"
+              className="flex items-center text-sm text-gray-500 hover:underline"
+            >
+              <svg
+                aria-hidden="true"
+                className="mr-1 w-4 h-4"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+                ></path>
+              </svg>
+              Reply
+            </button>
+          </div>
+        </article>
+        <article className="p-6 text-base bg-white border-t border-gray-200">
+          <footer className="flex justify-between items-center mb-2">
+            <div className="flex items-center">
+              <p className="inline-flex items-center mr-3 text-sm text-gray-900">
+                <img
+                  className="mr-2 w-6 h-6 rounded-full"
+                  src="https://flowbite.com/docs/images/people/profile-picture-4.jpg"
+                  alt="Helene Engels"
+                />
+                Helene Engels
+              </p>
+              <p className="text-sm text-gray-600">
+                <time dateTime="2022-06-23" title="June 23rd, 2022">
+                  Jun. 23, 2022
+                </time>
+              </p>
+            </div>
+            <button
+              id="dropdownComment4Button"
+              data-dropdown-toggle="dropdownComment4"
+              className="inline-flex items-center p-2 text-sm font-medium text-center text-gray-400 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50"
+              type="button"
+            >
+              <svg
+                className="w-5 h-5"
+                aria-hidden="true"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z"></path>
+              </svg>
+            </button>
+            <div
+              id="dropdownComment4"
+              className="hidden z-10 w-36 bg-white rounded divide-y divide-gray-100 shadow"
+            >
+              <ul
+                className="py-1 text-sm text-gray-700"
+                aria-labelledby="dropdownMenuIconHorizontalButton"
+              >
+                <li>
+                  <a href="#" className="block py-2 px-4 hover:bg-gray-100">
+                    Edit
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="block py-2 px-4 hover:bg-gray-100">
+                    Remove
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="block py-2 px-4 hover:bg-gray-100">
+                    Report
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </footer>
+          <p className="text-gray-500">
+            Thanks for sharing this. I do came from the Backend development and
+            explored some of the tools to design my Side Projects.
+          </p>
+          <div className="flex items-center mt-4 space-x-4">
+            <button
+              type="button"
+              className="flex items-center text-sm text-gray-500 hover:underline"
+            >
+              <svg
+                aria-hidden="true"
+                className="mr-1 w-4 h-4"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+                ></path>
+              </svg>
+              Reply
+            </button>
+          </div>
+        </article>
+      </div>
+    </section>
+  );
+}
+function EditInactiveIcon(props: { className: string }) {
+  return (
+    <svg
+      {...props}
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M4 13V16H7L16 7L13 4L4 13Z"
+        fill="#FFFFFF"
+        stroke="#FDE047"
+        strokeWidth="2"
+      />
+    </svg>
+  );
+}
+
+function EditActiveIcon(props) {
+  return (
+    <svg
+      {...props}
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M4 13V16H7L16 7L13 4L4 13Z"
+        fill="#8B5CF6"
+        stroke="#C4B5FD"
+        strokeWidth="2"
+      />
+    </svg>
+  );
+}
+
+function DeleteInactiveIcon(props) {
+  return (
+    <svg
+      {...props}
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect
+        x="5"
+        y="6"
+        width="10"
+        height="10"
+        fill="#FFFFFF"
+        stroke="#FDE047"
+        strokeWidth="2"
+      />
+      <path d="M3 6H17" stroke="#FDE047" strokeWidth="2" />
+      <path d="M8 6V4H12V6" stroke="#FDE047" strokeWidth="2" />
+    </svg>
+  );
+}
+
+function DeleteActiveIcon(props) {
+  return (
+    <svg
+      {...props}
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect
+        x="5"
+        y="6"
+        width="10"
+        height="10"
+        fill="#8B5CF6"
+        stroke="#C4B5FD"
+        strokeWidth="2"
+      />
+      <path d="M3 6H17" stroke="#C4B5FD" strokeWidth="2" />
+      <path d="M8 6V4H12V6" stroke="#C4B5FD" strokeWidth="2" />
+    </svg>
+  );
+}
