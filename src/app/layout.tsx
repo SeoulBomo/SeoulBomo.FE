646d29d4f9477cfc4c325a475a329519695514e5
김태강 <k_2684@naver.com>
2023-05-27 21:29:56 +0900

    fix: 카카오맵 API 스크립트 이동

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Pretendard } from "@/utils/font";
 import "./globals.css";
 import NextProvider from "./NextProvider";
+import Script from "next/script";
 
 export const metadata = {
   title: "서울보모",
@@ -12,9 +13,11 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const kakaoUrl = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_APP_JS_KEY}&autoload=false`;
   return (
     <html lang="ko">
       <body className={Pretendard.className}>
+        <Script src={kakaoUrl} strategy="beforeInteractive" />
         <NextProvider>
           <div className="bg-cardFrameColor">{children}</div>
         </NextProvider>
--- src/components/detail/KakaoMap.tsx
+++ src/components/detail/KakaoMap.tsx
@@ -1,14 +1,10 @@
-"use client";
-import Script from "next/script";
 import { Map, MapMarker } from "react-kakao-maps-sdk";
 
 export default function KakaoMap({ lat, lng }: { lat: number; lng: number }) {
-  const kakaoUrl = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_APP_JS_KEY}&autoload=false`;
-
+  console.log(lat, lng);
   return (
     <>
       {/* eslint-disable-next-line @next/next/no-before-interactive-script-outside-document */}
-      <Script src={kakaoUrl} strategy="beforeInteractive" />
       <Map
         className="lg:px-[19.5rem] py-[10rem]"
         center={{ lat: lat, lng: lng }}
