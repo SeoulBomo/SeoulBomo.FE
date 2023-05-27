02d272b354238373bc9d91dbc0acb42c9d889884
김태강 <k_2684@naver.com>
2023-05-27 18:04:23 +0900

    feat: atom type 추가

--- src/state/index.ts
+++ src/state/index.ts
@@ -9,28 +9,31 @@ const { persistAtom: tokenPersistAtom } = recoilPersist({
   storage: sessionStorage,
 });
 
-export const userAtom = atom({
+export interface IUserTypes {
+  id: string;
+  accessToken: string;
+  socialType: string;
+  profileImage: string;
+  name: string;
+}
+
+export const userAtom = atom<IUserTypes | undefined>({
   key: "userAtom",
   default: undefined,
   effects_UNSTABLE: [tokenPersistAtom],
 });
 
-export const isLoginSelector = selector({
+export const isLoginSelector = selector<boolean>({
   key: "isLoginSelector",
   get: ({ get }) => !!get(userAtom),
 });
 
-export const kakaoTokenAtom = atom({
+export const kakaoTokenAtom = atom<undefined | string>({
   key: "kakaoTokenAtom",
   default: undefined,
 });
 
-export const naverTokenAtom = atom({
+export const naverTokenAtom = atom<undefined | string>({
   key: "naverTokenAtom",
   default: undefined,
-});
-
-export const filterState = atom({
-  key: "filter",
-  default: {},
 });
