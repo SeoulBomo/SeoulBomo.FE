fc283a47d1457ee23f6dd702d0744e5c82a82821
김태강 <k_2684@naver.com>
2023-05-29 03:21:03 +0900

    chore: news 카드 캐러셀 사파리 크로스브라우징 해결

--- src/components/main/CardSection/NewsCard.tsx
+++ src/components/main/CardSection/NewsCard.tsx
@@ -12,7 +12,7 @@ export default function NewsCard() {
       <text className="font-bold text-[1.5rem] lg:text-[1.75rem] mb-[1rem]">
         복지 소식
       </text>
-      <div className="flex flex-col justify-evenly w-[14rem] h-[22rem] lg:w-[18rem] lg:h-[28rem] p-[0.5rem] rounded-[1rem] bg-white">
+      <div className="flex flex-col justify-evenly w-[14rem] lg:w-[18rem] h-[25rem] p-[0.5rem] rounded-[1rem] bg-white">
         <text className="font-medium text-lg lg:text-2xl truncate hover:underline underline-offset-4 cursor-pointer">
           <Swiper
             effect={"cards"}
