bdc62234b8b066a7a3d2a0136ab9e87418cf5343
김태강 <k_2684@naver.com>
2023-05-31 01:22:51 +0900

    feat: metadata 추가

---
+++ src/app/(header)/error/layout.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "서울보모 : 에러페이지",
+  description: "서울보모 에러 페이지",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div>{children}</div>;
+}
---
+++ src/app/(non-header)/login/layout.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "서울보모 : 로그인",
+  description: "서울보모 로그인 페이지",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div>{children}</div>;
+}
