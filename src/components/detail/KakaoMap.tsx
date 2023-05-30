13d7d6f9652719235b064a8e836adf9d09233211
김태강 <k_2684@naver.com>
2023-05-30 17:36:15 +0900

    fix: map useEffect 후 생성 제어

--- src/components/detail/KakaoMap.tsx
+++ src/components/detail/KakaoMap.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Map, MapMarker } from "react-kakao-maps-sdk";
 
 export default function KakaoMap({
@@ -9,16 +9,26 @@ export default function KakaoMap({
   lat: number | undefined;
   lng: number | undefined;
 }) {
+  const [isHydrated, setIsHydrated] = useState(false);
+  useEffect(() => {
+    setIsHydrated(true);
+  }, [lat, lng]);
+
   return (
     <>
       {/* eslint-disable-next-line @next/next/no-before-interactive-script-outside-document */}
-      <Map
-        className="px-[0rem] sm:px-[15rem] md:px-[25rem] lg:px-[32rem] py-[10rem]"
-        center={{ lat: lat ?? 33.5563, lng: lng ?? 126.795841 }}
-        level={1}
-      >
-        <MapMarker position={{ lat: lat ?? 33.5563, lng: lng ?? 126.795841 }} />
-      </Map>
+      {isHydrated ? (
+        <Map
+          onCreate={(map) => console.log(map)}
+          className="px-[0rem] sm:px-[15rem] md:px-[25rem] lg:px-[32rem] py-[10rem]"
+          center={{ lat: lat ?? 33.5563, lng: lng ?? 126.795841 }}
+          level={1}
+        >
+          <MapMarker
+            position={{ lat: lat ?? 33.5563, lng: lng ?? 126.795841 }}
+          />
+        </Map>
+      ) : null}
     </>
   );
 }
