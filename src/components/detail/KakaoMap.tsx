15db0fa01023ded97f40662c17024cd5e4a6064b
김태강 <k_2684@naver.com>
2023-05-30 19:34:06 +0900

    fix: useInjectKakaoMapApi 추가

--- src/components/detail/KakaoMap.tsx
+++ src/components/detail/KakaoMap.tsx
@@ -1,6 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
-import { Map, MapMarker } from "react-kakao-maps-sdk";
+import { Map, MapMarker, useInjectKakaoMapApi } from "react-kakao-maps-sdk";
 
 export default function KakaoMap({
   lat,
@@ -9,26 +8,19 @@ export default function KakaoMap({
   lat: number | undefined;
   lng: number | undefined;
 }) {
-  const [isHydrated, setIsHydrated] = useState(false);
-  useEffect(() => {
-    setIsHydrated(true);
-  }, [lat, lng]);
-
+  const { loading, error } = useInjectKakaoMapApi({
+    appkey: `${process.env.NEXT_PUBLIC_KAKAO_APP_JS_KEY}`,
+  });
   return (
     <>
       {/* eslint-disable-next-line @next/next/no-before-interactive-script-outside-document */}
-      {isHydrated ? (
-        <Map
-          onCreate={(map) => console.log(map)}
-          className="px-[0rem] sm:px-[15rem] md:px-[25rem] lg:px-[32rem] py-[10rem]"
-          center={{ lat: lat ?? 33.5563, lng: lng ?? 126.795841 }}
-          level={1}
-        >
-          <MapMarker
-            position={{ lat: lat ?? 33.5563, lng: lng ?? 126.795841 }}
-          />
-        </Map>
-      ) : null}
+      <Map
+        className="px-[0rem] sm:px-[15rem] md:px-[25rem] lg:px-[32rem] py-[10rem]"
+        center={{ lat: lat ?? 33.5563, lng: lng ?? 126.795841 }}
+        level={1}
+      >
+        <MapMarker position={{ lat: lat ?? 33.5563, lng: lng ?? 126.795841 }} />
+      </Map>
     </>
   );
 }
