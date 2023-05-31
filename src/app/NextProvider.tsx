374de6cd462ccc1329790065940662f12c592f2a
김태강 <k_2684@naver.com>
2023-05-31 16:04:18 +0900

    fix: kakao script 문제 수정

--- src/app/NextProvider.tsx
+++ src/app/NextProvider.tsx
@@ -1,20 +1,27 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { PropsWithChildren, useEffect, useState } from "react";
+import Script from "next/script";
+import { PropsWithChildren, useState } from "react";
 import { useInjectKakaoMapApi } from "react-kakao-maps-sdk";
 import { RecoilRoot } from "recoil";
 
 export default function NextProvider({ children }: PropsWithChildren) {
   const [queryClient] = useState(() => new QueryClient());
 
-  const { loading, error } = useInjectKakaoMapApi({
-    appkey: `${process.env.NEXT_PUBLIC_KAKAO_APP_JS_KEY}`,
-  });
+  // const { loading, error } = useInjectKakaoMapApi({
+  //   appkey: `${process.env.NEXT_PUBLIC_KAKAO_APP_JS_KEY}`,
+  // });
 
   return (
     <QueryClientProvider client={queryClient}>
-      <RecoilRoot>{children}</RecoilRoot>
+      <RecoilRoot>
+        <Script
+          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_APP_JS_KEY}&libraries=services,clusterer&autoload=false`}
+          strategy="beforeInteractive"
+        />
+        {children}
+      </RecoilRoot>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
